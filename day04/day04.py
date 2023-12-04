ffd0a109489ae78f1951211fe6388544b72e8dc6
ElliotRedhead <elliot.redhead@gmail.com>
2023-12-04 20:46:25 +0000

    Python for day 4 pt.1

--- day04/day04.py
+++ day04/day04.py
@@ -8,29 +8,53 @@ def extract_rows(input: str) -> list[str]:
     return split
 
 
-def extract_numbers(line: str) -> list[str]:
-    matches = re.compile(r"\b(\d{1,2})\s").findall(line)
-    nums = []
+def extract_numbers(line: str) -> list[list[int]]:
+    matches = re.compile(r"\b(\d{1,2})\s?|\|").findall(line)
+    matches.pop(0)
+
+    parsed_scratchcard: list[list[int]] = [[], []]
+    target_array = 0
     for match in matches:
-        nums.append(int(match))
+        try:
+            scratch_num = int(match)
+        except ValueError:
+            target_array = 1
+            continue
+        parsed_scratchcard[target_array].append(int(scratch_num))
+    return parsed_scratchcard
 
-    return matches
+
+def intersecting_list_values(list_one: list, list_two: list) -> list:
+    return list(set(list_one) & set(list_two))
 
 
-def calculate_scratchcard_points(scratchcards_file) -> int:
-    sum_points: int = 0
+def calculate_line_points(intersecting_count: int) -> int:
+    line_score = 0
+    if intersecting_count == 1:
+        line_score = 1
+    if intersecting_count > 1:
+        line_score = 1
+        for _ in range(intersecting_count - 1):
+            line_score = line_score * 2
+    return line_score
+
+
+def calculate_scratchcards_points(scratchcards_file) -> int:
     lines: list[str] = extract_rows(scratchcards_file)
-    separator_index: int = lines[0].find("|")
 
-    for line in lines:
-        extract_numbers(line)
+    total_score = 0
+    for _, line in enumerate(lines):
+        scratch_nums = extract_numbers(line)
+        intersecting_values = intersecting_list_values(scratch_nums[0], scratch_nums[1])
+        scratchcard_score = calculate_line_points(len(intersecting_values))
+        total_score += scratchcard_score
 
-    return sum_points
+    return total_score
 
 
 if __name__ == "__main__":  # pragma: no cover
     current_file_path = os.path.realpath(__file__)
     current_directory = os.path.dirname(current_file_path)
-    scratchcards_file = open(os.path.join(current_directory, "example.txt"), "r").read()
-    ans_one = calculate_scratchcard_points(scratchcards_file)
-    print(f"Answer to first puzzle is: {ans_one}")
+    scratchcards_file = open(os.path.join(current_directory, "input.txt"), "r").read()
+    ans_one = calculate_scratchcards_points(scratchcards_file)
+    print(f"Answer to first puzzle is: {ans_one}")  # 20855
--- day04/test_day04.py
+++ day04/test_day04.py
@@ -1,6 +1,13 @@
 import os
 
-from day04.day04 import calculate_scratchcard_points
+import pytest
+
+from day04.day04 import (
+    calculate_line_points,
+    calculate_scratchcards_points,
+    extract_numbers,
+    intersecting_list_values,
+)
 
 current_file_path = os.path.realpath(__file__)
 current_directory = os.path.dirname(current_file_path)
@@ -8,5 +15,42 @@ current_directory = os.path.dirname(current_file_path)
 example_doc = open(os.path.join(current_directory, "example.txt"), "r").read()
 
 
+@pytest.mark.parametrize(
+    ("scratch_line", "expected"),
+    [
+        pytest.param(
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            [[41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]],
+        )
+    ],
+)
+def test_extract_numbers(scratch_line, expected):
+    assert extract_numbers(scratch_line) == expected
+
+
+def test_intersecting_list_values():
+    list_one = [41, 48, 83, 86, 17]
+    list_two = [83, 86, 6, 31, 17, 9, 48, 53]
+    assert (intersecting_list_values(list_one, list_two)) == [48, 17, 83, 86]
+
+
+@pytest.mark.parametrize(
+    ("intersecting_count", "expected"),
+    [
+        pytest.param(0, 0),
+        pytest.param(1, 1),
+        pytest.param(2, 2),
+        pytest.param(3, 4),
+        pytest.param(4, 8),
+        pytest.param(5, 16),
+        pytest.param(6, 32),
+        pytest.param(7, 64),
+        pytest.param(8, 128),
+    ],
+)
+def test_calculate_line_points(intersecting_count, expected):
+    assert (calculate_line_points(intersecting_count)) == expected
+
+
 def test_calculate_scratchard_points():
-    assert (calculate_scratchcard_points(example_doc)) == 13
+    assert (calculate_scratchcards_points(example_doc)) == 13
